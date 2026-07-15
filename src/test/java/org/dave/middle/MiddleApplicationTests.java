fcf5dab9c551d63a414e983d3957de88928bc6b5
Davlat <davlatbek.muxtorov@octo.uz>
2026-07-15 17:51:22 +0500

    init commit

---
+++ src/test/java/org/dave/middle/MiddleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dave.middle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
